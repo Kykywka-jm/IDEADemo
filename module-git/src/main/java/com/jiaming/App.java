0cbf778e7955552f7f6c5a84402b1d1cbb19f051
Tourbillon <619199932@qq.com>
2023-01-12 15:03:11 +0800

    push

--- module-git/src/main/java/com/jiaming/App.java
+++ module-git/src/main/java/com/jiaming/App.java
@@ -10,7 +10,7 @@ public class App {
         System.out.println( "Hello World2" );
         System.out.println( "Hello World3" );
         System.out.println( "Hello World4" );
-        System.out.println( "master" );
-        System.out.println( "hot-fix" );
+        System.out.println( "push test" );
+
     }
 }
